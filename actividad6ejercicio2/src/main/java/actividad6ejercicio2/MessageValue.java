cba298fc1bfe1844c4fcb90687a305e1ce40705e
Damian Ferro <dfmartinez8909@gmail.com>
2021-12-12 01:12:49 +0100

    Actividad6Ejercicio2

---
+++ actividad6ejercicio2/src/main/java/actividad6ejercicio2/MessageValue.java
@@ -0,0 +1,13 @@
+package actividad6ejercicio2;
+
+import org.mule.api.MuleEventContext;
+import org.mule.api.lifecycle.Callable;
+
+public class MessageValue implements Callable {
+	public Object onCall(MuleEventContext eventContext)throws Exception{
+		//Se divide el mensaje para devolver solamente el campo value
+		 String valueArray[] = eventContext.getMessage().getPayloadAsString().split(",");
+		 return valueArray[1];
+	}
+
+}
