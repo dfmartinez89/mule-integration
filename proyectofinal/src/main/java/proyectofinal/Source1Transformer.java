dac7ffcdf83ee6c52028c9b3a09df6745dd3ecc6
Damian Ferro <dfmartinez8909@gmail.com>
2022-01-08 23:46:03 +0100

    Proyecto Final Fuente 1

---
+++ proyectofinal/src/main/java/proyectofinal/Source1Transformer.java
@@ -0,0 +1,26 @@
+package proyectofinal;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+import org.mule.api.MuleEventContext;
+import org.mule.api.lifecycle.Callable;
+
+public class Source1Transformer implements Callable {
+	String timestamp;
+
+	public Object onCall(MuleEventContext eventContext) throws Exception {
+		String valueArray[] = eventContext.getMessage().getPayloadAsString().split(",");
+		conversionDate(); // se obtiene timestamp de la conversión
+		return valueArray[5].replace("\"quotes\":", "").replace("}}", "").trim()
+				.concat(",\"DATE\":" + timestamp + "}");
+
+	}
+
+	public void conversionDate() {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("DD-MM-YYYY hh:mm:ss");
+		Date date = new Date();
+		timestamp = dateFormat.format(date);
+		;
+	}
+}
